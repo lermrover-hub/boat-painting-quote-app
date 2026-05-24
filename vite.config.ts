78b89b3b9295e7bb893cb59d6e05afb1d0cb84e5
lermrover-hub <lermrover@gmail.com>
2026-05-24 15:31:10 +0700

    Restore GitHub Pages repo path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/boat-paintquoat-app/",
+  base: "/boat-painting-quote-app/",
   plugins: [react()],
   build: {
     sourcemap: true
